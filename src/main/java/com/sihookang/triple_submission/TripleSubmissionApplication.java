425b909a2ad3628ae6cc0418bc43b0ab71ba7264
mikekang47 <gyeong0308@gmail.com>
2022-06-26 19:50:33 +0900

    Set environment

---
+++ src/main/java/com/sihookang/triple_submission/TripleSubmissionApplication.java
@@ -0,0 +1,13 @@
+package com.sihookang.triple_submission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleSubmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripleSubmissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sihookang/triple_submission/TripleSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sihookang.triple_submission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleSubmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
