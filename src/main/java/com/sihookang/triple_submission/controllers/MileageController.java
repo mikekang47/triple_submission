5a45421fe3088d574e60a8bfa13e16f4aae10452
mikekang47 <gyeong0308@gmail.com>
2022-06-26 20:31:15 +0900

    Add: create MileageController class

---
+++ src/main/java/com/sihookang/triple_submission/controllers/MileageController.java
@@ -0,0 +1,12 @@
+package com.sihookang.triple_submission.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/events")
+public class MileageController {
+
+
+}
