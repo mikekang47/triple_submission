b5ea3ff15d52aa574a889e0ad899cb4efe36aabb
mikekang47 <gyeong0308@gmail.com>
2022-06-26 19:59:52 +0900

    Add: create Review domain

---
+++ src/main/java/com/sihookang/triple_submission/domain/Review.java
@@ -0,0 +1,29 @@
+package com.sihookang.triple_submission.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "REVIEW")
+public class Review {
+    @Id
+    @Column(name = "REVIEW_ID", nullable = false)
+    private UUID id;
+
+    @Column(name = "CONTENT")
+    private String content;
+
+
+}
