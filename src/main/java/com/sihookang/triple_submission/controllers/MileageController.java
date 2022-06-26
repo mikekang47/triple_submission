4ec361bc0a347b7979900daed132f63e26f46ea8
mikekang47 <gyeong0308@gmail.com>
2022-06-26 21:58:04 +0900

    Add: create "create" Method in MileageController class

--- src/main/java/com/sihookang/triple_submission/controllers/MileageController.java
+++ src/main/java/com/sihookang/triple_submission/controllers/MileageController.java
@@ -1,12 +1,26 @@
 package com.sihookang.triple_submission.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sihookang.triple_submission.applications.MileageService;
+import com.sihookang.triple_submission.domain.Mileage;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/events")
 public class MileageController {
+    private final MileageService mileageService;
 
+    public MileageController(MileageService mileageService) {
+        this.mileageService = mileageService;
+    }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mileage create() {
+        return null;
+    }
 }
