79ff4957027c02fecad70a876ecaea42d7d4e3e3
mikekang47 <gyeong0308@gmail.com>
2022-06-27 11:43:14 +0900

    Add: add userService field in MileageController Add: user in create method

--- src/main/java/com/sihookang/triple_submission/controllers/MileageController.java
+++ src/main/java/com/sihookang/triple_submission/controllers/MileageController.java
@@ -2,17 +2,22 @@ package com.sihookang.triple_submission.controllers;
 
 
 import com.sihookang.triple_submission.applications.MileageService;
+import com.sihookang.triple_submission.applications.UserService;
 import com.sihookang.triple_submission.domain.Mileage;
+import com.sihookang.triple_submission.domain.User;
+import com.sihookang.triple_submission.dto.MileageData;
+import com.sihookang.triple_submission.errors.InvalidTypeException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/events")
 public class MileageController {
     private final MileageService mileageService;
+    private UserService userService;
 
     public MileageController(MileageService mileageService) {
         this.mileageService = mileageService;
@@ -20,7 +25,13 @@ public class MileageController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Mileage create() {
-        return null;
+    public Mileage create(@RequestBody @Valid MileageData mileageData) {
+        String type = mileageData.getType();
+        if(!type.equals("REVIEW")) {
+            throw new InvalidTypeException(type);
+        }
+        User user = userService.getUser(mileageData.getUserId());
+
+        return new Mileage();
     }
 }
