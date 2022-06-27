2d56741bd942377cadadd08cc7d76f11cd61e33a
mikekang47 <gyeong0308@gmail.com>
2022-06-27 11:44:57 +0900

    Add: create createMileage method in MileageService

--- src/main/java/com/sihookang/triple_submission/applications/MileageService.java
+++ src/main/java/com/sihookang/triple_submission/applications/MileageService.java
@@ -1,8 +1,21 @@
 package com.sihookang.triple_submission.applications;
 
+import com.sihookang.triple_submission.domain.*;
+import com.sihookang.triple_submission.infra.MileageRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MileageService {
+    private final MileageRepository mileageRepository;
 
+    public MileageService(MileageRepository mileageRepository) {
+        this.mileageRepository = mileageRepository;
+    }
+
+    public Mileage createMileage() {
+        
+        return null;
+    }
 }
