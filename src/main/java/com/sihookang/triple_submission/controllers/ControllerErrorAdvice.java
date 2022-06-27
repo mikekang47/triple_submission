cbf8c6afd20f977471bcbbae7afc1f068446e55f
mikekang47 <gyeong0308@gmail.com>
2022-06-27 14:06:48 +0900

    Add: add handler for UserNotFounxException and PlaceNotFoundException in ControllerErrorAdvice

--- src/main/java/com/sihookang/triple_submission/controllers/ControllerErrorAdvice.java
+++ src/main/java/com/sihookang/triple_submission/controllers/ControllerErrorAdvice.java
@@ -2,6 +2,8 @@ package com.sihookang.triple_submission.controllers;
 
 import com.sihookang.triple_submission.dto.ErrorResponse;
 import com.sihookang.triple_submission.errors.InvalidTypeException;
+import com.sihookang.triple_submission.errors.PlaceNotFoundException;
+import com.sihookang.triple_submission.errors.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,6 +18,18 @@ public class ControllerErrorAdvice {
     @ExceptionHandler(InvalidTypeException.class)
     public ErrorResponse handleInvalidTypeException() {
         return new ErrorResponse("Invalid Type");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public ErrorResponse handleUserNotFoundException() {
+        return new ErrorResponse("User not found");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(PlaceNotFoundException.class)
+    public ErrorResponse handlePlaceNotFoundException() {
+        return new ErrorResponse("Place not found");
     }
 
 }
