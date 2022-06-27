cd85c08f99e584c051e57ac19af9bede2ffbf962
mikekang47 <gyeong0308@gmail.com>
2022-06-27 15:42:23 +0900

    Add: create getPlace method in PlaceService

--- src/main/java/com/sihookang/triple_submission/applications/PlaceService.java
+++ src/main/java/com/sihookang/triple_submission/applications/PlaceService.java
@@ -1,7 +1,23 @@
 package com.sihookang.triple_submission.applications;
 
+import com.sihookang.triple_submission.domain.Place;
+import com.sihookang.triple_submission.errors.PlaceNotFoundException;
+import com.sihookang.triple_submission.infra.PlaceRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class PlaceService {
+    private final PlaceRepository placeRepository;
+
+
+    public PlaceService(PlaceRepository placeRepository) {
+        this.placeRepository = placeRepository;
+    }
+
+    public Place getPlace(UUID id) {
+        return placeRepository.findById(id)
+                .orElseThrow(() -> new PlaceNotFoundException(id));
+    }
 }
