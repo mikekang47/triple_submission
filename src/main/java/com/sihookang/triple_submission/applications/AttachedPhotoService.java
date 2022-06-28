3bc95d83da7b382c51f1db059a8106b37a88df4b
mikekang47 <gyeong0308@gmail.com>
2022-06-28 19:32:13 +0900

    Modify: change return type attahcedPhoto to List type in AttachedPhotoService

--- src/main/java/com/sihookang/triple_submission/applications/AttachedPhotoService.java
+++ src/main/java/com/sihookang/triple_submission/applications/AttachedPhotoService.java
@@ -5,7 +5,9 @@ import com.sihookang.triple_submission.errors.PhotoNotFoundException;
 import com.sihookang.triple_submission.infra.AttachedPhotoRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 /**
  * 리뷰에 첨부된 사진에 관련된 실제 처리를 담당하는 클래스입니다.
@@ -19,12 +21,12 @@ public class AttachedPhotoService {
     }
 
     /**
-     * parameter id와 일치하는 id를 가진 사진 객체를 찾아서 반환합니다.
-     * @param id 찾고자 하는 id
+     * parameter id list와 일치하는 id를 가진 사진 객체를 찾아서 반환합니다.
+     * @param ids 찾고자 하는 id list
      * @return parameter id와 일치하는 id를 가진 사진 객체
      */
-    public AttachedPhoto getPhoto(UUID id) {
-        return attachedPhotoRepository.findById(id)
-                .orElseThrow(() -> new PhotoNotFoundException(id));
+    public List<AttachedPhoto> getPhotos(List<UUID> ids) {
+        return ids.stream().map(id -> attachedPhotoRepository.findById(id)
+                .orElseThrow(() -> new PhotoNotFoundException(id))).collect(Collectors.toList());
     }
 }
