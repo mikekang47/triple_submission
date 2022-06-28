0c33d9efec0d34ea35597f29883ed48af9917d95
mikekang47 <gyeong0308@gmail.com>
2022-06-28 13:40:06 +0900

    Add: add javadoc in AttachedPhotoService

--- src/main/java/com/sihookang/triple_submission/applications/AttachedPhotoService.java
+++ src/main/java/com/sihookang/triple_submission/applications/AttachedPhotoService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+/**
+ * 리뷰에 첨부된 사진에 관련된 실제 처리를 담당하는 클래스입니다.
+ */
 @Service
 public class AttachedPhotoService {
     private final AttachedPhotoRepository attachedPhotoRepository;
@@ -14,7 +17,12 @@ public class AttachedPhotoService {
     public AttachedPhotoService(AttachedPhotoRepository attachedPhotoRepository) {
         this.attachedPhotoRepository = attachedPhotoRepository;
     }
-    
+
+    /**
+     * parameter id와 일치하는 id를 가진 사진 객체를 찾아서 반환합니다.
+     * @param id 찾고자 하는 id
+     * @return parameter id와 일치하는 id를 가진 사진 객체
+     */
     public AttachedPhoto getPhoto(UUID id) {
         return attachedPhotoRepository.findById(id)
                 .orElseThrow(() -> new PhotoNotFoundException(id));
