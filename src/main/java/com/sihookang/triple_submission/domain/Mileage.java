129781852f7cb7b6c97a3752cf9e31c7027778d1
mikekang47 <gyeong0308@gmail.com>
2022-06-29 05:08:18 +0900

    Modify: change PK type Long to UUID in Mileage domain Add: add generator for JPA Add: add JSONBackReference

--- src/main/java/com/sihookang/triple_submission/domain/Mileage.java
+++ src/main/java/com/sihookang/triple_submission/domain/Mileage.java
@@ -1,29 +1,38 @@
 package com.sihookang.triple_submission.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.UUID;
 
 
 @NoArgsConstructor
 @Getter
 @Entity
+@Setter
 @Table(name = "MILEAGE")
 public class Mileage extends BaseTimeEntity{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ID", nullable = false)
-    private Long id;
+    @GeneratedValue(generator = "UUID")
+    @Column(name = "ID", nullable = false, columnDefinition = "BINARY(16)")
+    @GenericGenerator(
+            name="UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    private UUID id;
 
     @Column(name = "type")
     private String type;
 
     @OneToOne
     @JoinColumn(name = "REVIEW_ID")
+    @JsonBackReference
     private Review review;
 
     @Builder
-    public Mileage(Long id, String type, Review review) {
+    public Mileage(UUID id, String type, Review review) {
         this.id = id;
         this.type = type;
         this.review = review;
