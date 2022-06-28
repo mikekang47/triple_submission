445e085e9e0a9aaa45198b6053974da457fa62db
mikekang47 <gyeong0308@gmail.com>
2022-06-28 13:53:40 +0900

    Add: add javadoc in UserService

--- src/main/java/com/sihookang/triple_submission/applications/UserService.java
+++ src/main/java/com/sihookang/triple_submission/applications/UserService.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+/**
+ * User와 관련된 내부 처리를 담당하는 클래스입니다.
+ */
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -15,6 +18,11 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * parameter로 입력된 찾으려고 하는 객체의 id와 같은 id를 가진 객체를 반환합니다.
+     * @param valid_id  찾으려고 하는 객체의 id
+     * @return  사용자 객체
+     */
     public User getUser(UUID valid_id) {
         return userRepository.findById(valid_id)
                 .orElseThrow(() -> new UserNotFoundException(valid_id));
