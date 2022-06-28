f9dd0633939b144e16044e48b325aaf6c3d5384c
mikekang47 <gyeong0308@gmail.com>
2022-06-28 13:58:23 +0900

    Add: add javadoc in ControllerErrorAdvice

--- src/main/java/com/sihookang/triple_submission/controllers/ControllerErrorAdvice.java
+++ src/main/java/com/sihookang/triple_submission/controllers/ControllerErrorAdvice.java
@@ -12,27 +12,46 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
+/**
+ *  에러와 관련된 HTTP 응답을 관리하는 클래스입니다.
+ */
 @ResponseBody
 @ControllerAdvice
 public class ControllerErrorAdvice {
+    /**
+     * InvalidTypeException이 발생했을 때 client에 에러코드 400을 반환하고 에러 메시지를 반환합니다.
+     * @return ErrorReponse 객체
+     */
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(InvalidTypeException.class)
     public ErrorResponse handleInvalidTypeException() {
         return new ErrorResponse("Invalid Type");
     }
 
+    /**
+     * UserNotFoundException이 발생했을 때 client에 에러코드 404를 반환하고 에러 메시지를 반환합니다.
+     * @return ErrorReponse 객체
+     */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
     public ErrorResponse handleUserNotFoundException() {
         return new ErrorResponse("User not found");
     }
 
+    /**
+     * PlaceNotFoundException이 발생했을 때 client에 에러코드 404를 반환하고 에러 메시지를 반환합니다.
+     * @return ErrorReponse 객체
+     */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(PlaceNotFoundException.class)
     public ErrorResponse handlePlaceNotFoundException() {
         return new ErrorResponse("Place not found");
     }
 
+    /**
+     * PhotoNotFoundException이 발생했을 때 client에 에러코드 404를 반환하고 에러 메시지를 반환합니다.
+     * @return ErrorReponse 객체
+     */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(PhotoNotFoundException.class)
     public ErrorResponse handlePhotoNotFoundException() {
