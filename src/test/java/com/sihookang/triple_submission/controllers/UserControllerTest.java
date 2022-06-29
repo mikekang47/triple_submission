78598a4c54be62d4a401919f7ef3c2cc50d5c0cb
mikekang47 <gyeong0308@gmail.com>
2022-06-30 07:06:16 +0900

    Test: add getPoint method in UserControllerTest

--- src/test/java/com/sihookang/triple_submission/controllers/UserControllerTest.java
+++ src/test/java/com/sihookang/triple_submission/controllers/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.sihookang.triple_submission.applications.UserService;
 import com.sihookang.triple_submission.domain.User;
 import com.sihookang.triple_submission.errors.UserNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,10 +16,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
 import java.util.UUID;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
@@ -48,6 +51,7 @@ class UserControllerTest {
     }
 
     @Test
+    @DisplayName("올바른 ID로 사용자를 조회하려하면 사용자 객체를 반환한다.")
     void getUserWithValidId() throws Exception {
         mvc.perform(get("/users/"+VALID_ID)
                 .accept(MediaType.APPLICATION_JSON)
@@ -56,6 +60,7 @@ class UserControllerTest {
     }
 
     @Test
+    @DisplayName("올바르지 ID로 사용자를 조회하려하면 에러를 던진다.")
     void getUserWithInvalidId() throws Exception {
         mvc.perform(get("/users/"+INVALID_ID))
                 .andExpect(status().isNotFound());
@@ -63,10 +68,21 @@ class UserControllerTest {
     }
 
     @Test
+    @DisplayName("사용자를 생성하면 사용자를 반환한다.")
     void createUser() throws Exception {
         mvc.perform(post("/users"))
                 .andExpect(status().isCreated());
 
     }
+
+    @Test
+    @DisplayName("올바른 사용자의 ID로 총점을 조회하면 사용자의 총점을 반환한다.")
+    void getUserPointWithValidId() throws Exception {
+        mvc.perform(get("/users/points/"+VALID_ID))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("10")));
+    }
+
+
 
 }
